7063dd4d1dc5eb826a915a76f5cabc3ea3d9b0c1
geluco <68168158+Geluco@users.noreply.github.com>
2026-03-17 17:22:52 +0100

    update

--- list/list.cpp
+++ list/list.cpp
@@ -16,7 +16,8 @@ void createEmptyList (list *list){//iniciamos los parametros
 	list -> n = 0;
 	for(int i = 0; i < MAX_N; i++){
 		list -> vectorTF[i] = 0;
-	}
+        	list->vector[i] = 0;   // Dato a cero
+    	}
 }
 
 bool isEmptyList (list list){
@@ -35,22 +36,38 @@ int lastElement (list list){
 	return list.vector[list.n];
 }
 
-int vectorMovement(list *list, int position){//mueve las posiciones del vector
+void vectorMovement(list *list, int position){//mueve las posiciones del vector a la derecha
 	for(int i = list -> n; i > position; i--){	
 		list -> vector[i] = list -> vector[i - 1];
 		list -> vectorTF[i] = list -> vectorTF[i - 1];
 	}
 	list -> n = next(list -> n);
-	return true;
+}
+
+void vectorMovementBack(list *list, int position) {//mueve las posiciones hacia la izquierda
+  	for (int i = position; i < list->n - 1; i++) {
+        	list->vector[i] = list->vector[i + 1];
+        	list->vectorTF[i] = list->vectorTF[i + 1];
+	}
+	list -> n = previous(list -> n);
+}
+
+void lastNumberCheck (list *list){//comprueba que la posicion anterior a la ultima tenga un valor valido, sino, retrcede uno
+	while (true){
+		if(list -> vectorTF[previous(list -> n)] == 0){
+			list -> n = previous(list -> n);
+		}else{break;}
+	}
+
 }
 
 bool addItem (list *list, int item, int position){
-	if(!isFullList (*list)){
+	if(!isFullList(*list)){
 		if(position > MAX_N){//añadir al final
-
 		      	list -> vectorTF[list -> n] = 1;//cambiamos el estado de esta posicion a que esta ocupada por un valor	
 			list -> vector[list -> n] = item;
 			list -> n = next(list -> n);
+			lastNumberCheck(list);
 			std::cout << list -> n << " <-- valor de n \n";//debug
 			return true;
 
@@ -61,6 +78,7 @@ bool addItem (list *list, int item, int position){
 			}
 			list -> vectorTF[0] = 1;//cambiamos el esta a ocupado
 			list -> vector[0] = item;
+			lastNumberCheck(list);
 			std::cout << list -> n << " <-- valor de n \n";
 			
 
@@ -84,6 +102,7 @@ bool addItem (list *list, int item, int position){
 
 			list -> vectorTF[position] = 1;//pasara a esta ocupado
 			list -> vector[position] = item;
+			lastNumberCheck(list);
 			std::cout << list -> n << " <-- valor de n \n";
 			return true;
 		}
@@ -95,3 +114,59 @@ bool addItem (list *list, int item, int position){
 int returnItem (list list, int position){	
 	return list.vector[position];
 }
+
+int deleteItem (list *list, int position) { 
+	if(!isFullList(*list)){
+		if(position > MAX_N){//eliminar al final
+			if(list -> vectorTF[previous(list -> n)] == 1){//si esta ocupado
+				list -> vectorTF[previous(list -> n)] = 0;//cambiamos el estado de esta posicion a que no esta ocupada por un valor	
+				list -> vector[previous(list -> n)] = 0;//eliminamos el valor que queremos
+				vectorMovementBack(list, position);
+				lastNumberCheck(list);
+				std::cout << list -> n << " <-- valor de n \n";//debug
+				return true;
+			}else{//si no esta ocupado no hace nada
+				return false;
+			}
+
+		}else if(position < 0){//eliminar al principio
+
+			if(list -> vectorTF[0] == 1){//si esta ocupado
+				list -> vectorTF[0] = 0;//cambiamos el esta a ocupado
+				list -> vector[0] = 0;
+				vectorMovementBack(list, 0);
+				lastNumberCheck(list);
+				return true;	
+
+			}else{//si no esta ocupado simplemente aumentamos la posicion del ultimo valor
+				return false;
+
+			
+			std::cout << list -> n << " <-- valor de n \n";
+			return true;
+			}
+		}else{//eliminar en una posicion especifica
+
+			if(list -> n < position){//si la posicion esta por delante del ultimo valor añadido, este sera el nuevo ultimo valor
+				return false;
+			}
+			if(list -> vectorTF[position] == 1){//si esta ocupado
+				list -> vector[position] = 0;
+				list -> vectorTF[position] = 0;
+				vectorMovementBack(list, position);
+				lastNumberCheck(list);
+				return true;
+
+			}else{
+				return false;
+			}			
+			std::cout << list -> n << " <-- valor de n \n";
+			return true;		
+		}
+			
+	}
+	return 0; 
+}
+
+int update (list *list) { return 0; }
+int searchItem(list list, int item) { return 0; }
--- list/testlist.cpp
+++ list/testlist.cpp
@@ -6,21 +6,17 @@ int main (){
 	createEmptyList (&lista);
 
 	addItem (&lista, 11, MAX_N + 1);
-	addItem (&lista, 2, 10);
-	addItem (&lista, 3, 11);
-	addItem (&lista, 4, -1);
-	addItem (&lista, 5, 2);
-	addItem (&lista, 6, 99999999);
-	addItem (&lista, 7, -1);
-	addItem (&lista, 67, -1);
-	addItem (&lista, 99, 12);
-	addItem (&lista, 100, 5);
+	addItem (&lista, 2, 4);
 	addItem (&lista, 110, MAX_N + 1);
-	addItem (&lista, 230, MAX_N + 1);
-	addItem (&lista, 17, 6);
-	std::cout << firstElement (lista) << "\n";
-	for(int i = 0; i < 22; i++){	
+	addItem (&lista, 40, -1);
+	addItem (&lista, 40, MAX_N + 1);
+	deleteItem (&lista, 5);
+	deleteItem (&lista, MAX_N + 1);
+	deleteItem (&lista,  1);
+	std::cout << lastElement (lista) << "\n";
+	for(int i = 0; i < lista.n; i++){	
 	std::cout << returnItem (lista, i) <<"--- posicion --> " << i << " \n";
 	}
+	std::cout << lista.n; 
 	
 }
